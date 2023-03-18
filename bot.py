0fe81fd7e5f499b0f8a732616f594973a47584c4
ThomasTheFknTrainEngine <marten.uiboupin@gmail.com>
2023-03-18 16:28:28 +0100

    added code formating answers to some words

--- bot.py
+++ bot.py
@@ -22,6 +22,10 @@ async def ask_command(ctx, *, user_input):
     channel = ctx.channel.name
 
     bot_response = responses.send_responses(user_input)
+    symbol = "```"
+
+    if "code" or "program" or "python" or "pygame" or "javascript" or "html" or "c#" or "c++" or "js" or "php" in bot_response:
+        bot_response = symbol + bot_response + symbol
 
     await ctx.send(bot_response)
     print(f'{user_name} asked: {user_input} in #{channel}')
