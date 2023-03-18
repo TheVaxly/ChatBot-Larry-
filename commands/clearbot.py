2b613b149ecc94f908b3be54bceb9711890d1e80
ThomasTheFknTrainEngine <114987279+TheVaxly@users.noreply.github.com>
2023-03-18 21:27:22 +0200

    seperated the files

---
+++ commands/clearbot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.members = True
+
+client = commands.Bot(command_prefix='!', intents=intents)
+
+async def clear_command(ctx, amount: int):
+    bot_messages = []
+    async for message in ctx.channel.history(limit=100):
+        if message.author == client.user:
+            bot_messages.append(message)
+    to_delete = min(amount, len(bot_messages))
+    if to_delete <= 0:
+        await ctx.send("Invalid amount.")
+        return
+    for i in range(to_delete):
+        await bot_messages[i].delete()
+    print(f'{ctx.author.name} deleted {to_delete} bot messages')
