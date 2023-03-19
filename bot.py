0e1829825b26832364f8cb092601021fc8a9a993
Marten <marten.uiboupin@gmail.com>
2023-03-19 16:34:22 +0200

    changed the output of clearall

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ async def clear_all(ctx):
     async for message in ctx.channel.history(limit=None):
         if message.author == ctx.guild.me or message.author == ctx.author:
             await message.delete()
-    await ctx.send(f"All messages have been deleted.")
+    await ctx.send(f"``All messages have been deleted.``")
 
     print(f"{ctx.author.name} deleted all messages.")
 
