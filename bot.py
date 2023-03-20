d2a09dfbf3df8629d337d36e700e3c0f131733c8
ThomasTheFknTrainEngine <marten.uiboupin@gmail.com>
2023-03-20 20:43:48 +0100

    bug fixes

--- bot.py
+++ bot.py
@@ -48,20 +48,16 @@ async def help_command(ctx):
 
 @client.command(name="clearall")
 async def clear_all(ctx):
-    # Send a warning message before executing the command
     warning_msg = await ctx.send("Are you sure you want to delete all messages in this channel? This action cannot be undone. Type ``!yes`` to confirm.")
     try:
-        # Wait for a response from the user
         async def check(m):
             return m.author == ctx.author and m.channel == ctx.channel and m.content.lower() == "!yes"
         await client.wait_for('message', check=check, timeout=30)
     except asyncio.TimeoutError:
-        # If the user doesn't respond within 30 seconds, cancel the command
         await warning_msg.delete()
         await ctx.send("Command canceled. You did not confirm within 30 seconds.")
         return
     else:
-        # If the user confirms, delete all messages in the channel
         await warning_msg.delete()
     async for message in ctx.channel.history(limit=None):
         if message.author == ctx.guild.me or message.author == ctx.author:
