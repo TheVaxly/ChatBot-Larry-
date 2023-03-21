1e8650e9df389d381a3c97b99cee6b93d676eb3c
RaZe <razeslothc@gmail.com>
2023-03-22 01:21:32 +0200

    feat: delete button on reddit command

--- commands/reddit.py
+++ commands/reddit.py
@@ -3,12 +3,26 @@ import random
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import discord
 
 reddit = praw.Reddit(client_id=os.getenv('client_id'),
                     client_secret=os.getenv('client_secret'),
                     user_agent=os.getenv('user_agent'))
 
+
 async def meme(ctx, message):
+    class DeleteButton(discord.ui.View):
+        async def on_timeout(self):
+            for child in self.children:
+                child.disabled = True
+            await self.message.edit(view=self)
+        @discord.ui.button(label="Delete",style=discord.ButtonStyle.danger,custom_id=f"delete_{ctx.message.id}")
+        async def delete(self, interaction: discord.Interaction, button: discord.ui.Button):
+            if button.custom_id == f"delete_{ctx.message.id}":
+                await interaction.response.defer()
+                await interaction.message.delete()
+                self.stop()
+
     try:
         subreddit = reddit.subreddit(message)
         all_subs = []
@@ -24,8 +38,10 @@ async def meme(ctx, message):
                 continue
             else:
                 break
-
-        await ctx.send(f'``{random_sub.title}``\n{random_sub.url}')
-    except Exception:
+        view = DeleteButton(timeout=10 * 60)
+        
+        view.message = await ctx.send(content=f'``{random_sub.title}``\n{random_sub.url}', view=view)
+    except Exception as e:
+        print(f'Error: {e}')
         await ctx.send(f'``Invalid subreddit.``')
         return
